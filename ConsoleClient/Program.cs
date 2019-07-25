01769f4e8635fa8c1dbbb82280c83cb7822f9020
Sean Kelly <home@seank.com>
2019-07-25 08:00:58 -0700

    Initial Commit

---
+++ ConsoleClient/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleClient
+{
+    class Program
+    {
+        static async Task Doit()
+        {
+
+            HttpClient client = new HttpClient();
+
+            HttpResponseMessage response = await client.PostAsync("http://localhost:8080/data/", new StringContent(
+                "{\"key\": \"value\"}", Encoding.UTF8, "application/json"));
+
+            string content = await response.Content.ReadAsStringAsync();
+            Console.WriteLine("{0} {1}", response.StatusCode, content);
+        }
+
+        static void Main(string[] args)
+        {
+            Doit().Wait();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # dotnetcore-console-http-client
 A simple sample of a .Net Core console application that sends json to a server
+
+## Build
+
+Load solution in Visual Studio 2019 or later and press <kbd>F5</kbd>
+
+## Purpose
+
+This was a test app to prove that the code in the IoTEdge SensorModule would work.
